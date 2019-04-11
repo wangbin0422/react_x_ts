d0faa8b272fb038d36b3841d7cec91e6da6a6325
foreverwb <504543582@qq.com>
2019-04-11 17:17:49 +0800

    publish

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,19 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom';
-
-// import Button from './Button';
 import Icon from './Icon/Icon'
 
-const func:React.MouseEventHandler<SVGElement> = (e) => {
-  console.log(e, e.currentTarget.style);
-};
-
-ReactDOM.render(<div>
-  <Icon name="qq"/>
-  <Icon name="wechat"
-        className="wx"
-        onClick={func}
-        onMouseEnter={() => {console.log('enter')}}
-        onMouseLeave={() => {console.log('leave')}}/>
-</div>, document.querySelector('#root'));
-
+export {Icon}
