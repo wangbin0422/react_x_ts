5b49aeaffd5e679867d4c061304b6f715443d693
foreverwb <504543582@qq.com>
2019-04-22 17:16:39 +0800

    review

--- lib/untils/classes.ts
+++ lib/untils/classes.ts
@@ -12,26 +12,18 @@ interface ClassToggles {
   [K: string]: boolean
 }
 
-function scopedClassMaker(prefix: string) {
-  return function scopedClass(name: string | ClassToggles, options?: Options) {
-    const _name = (typeof name === 'string' || name === undefined) ?
-      {[name]: name} :
-      name;
-    const scoped = Object
-      .entries(_name)
+const scopedClassMaker = (prefix: string) =>
+  (name: string | ClassToggles, options?: Options) => {
+    return Object
+      .entries(name instanceof Object ? name : {[name]: name})
       .filter(entry => entry[1] !== false)
       .map(entry => entry[0])
-      .map(entry =>
-        [prefix, entry].filter(Boolean).join('-')
-      ).join(' ');
-
-    if (options && options.extra) {
-      return [scoped, options && options.extra].filter(Boolean).join(' ');
-    } else {
-      return scoped;
-    }
+      .map(entry => [prefix, entry]
+        .filter(Boolean)
+        .join('-'))
+      .concat(options && options.extra || [])
+      .join(' ');
   };
-}
 
 export {scopedClassMaker};
 
